d5f0e6c974d7eaafc3709e433127d50c61d703b9
bledpopkeki <bledh4352@gmail.com>
2025-01-23 16:17:48 +0100

    afsregedghderg

---
+++ php/moduli2/index.php
@@ -0,0 +1,32 @@
+<?php 
+$name="bledi ma i fort se profa";
+$age=16;
+$weight=59.5;
+$over_eighteen=false;
+echo $name."<br>";
+echo $age."<br> ";
+echo $weight. "<br> ";
+echo $over_eighteen. "<br> ";
+$x=5;
+$y=10;
+echo $x + $y;
+echo $x - $y;
+echo $x % $y;
+echo $x / $y;
+echo $x * $y;
+echo $x ** $y;
+echo strlen($name);
+echo strrev($name);
+echo str_word_count($name);
+echo str_replace("bledi", "profa", $name);
+
+
+
+
+
+
+
+
+
+
+?>
