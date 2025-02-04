4d9df5167933b5bdbe5ede3befb347918b7e618c
bledpopkeki <bledh4352@gmail.com>
2025-02-04 16:16:48 +0100

    asd

--- php/moduli4/index.php
+++ php/moduli4/index.php
@@ -1,11 +1,33 @@
 <?php 
+function max_numbers($x,$y){
+    if($x>$y){
+        echo($x);
+    }
+    elseif($x<$y){
+        echo($y);
+    }
+    else{
+        echo("-1");
+    }
+}
+max_numbers(8,8);
+function is_qift($m){
+    if($m%2>0){
+        echo "nuk eshte qift";
+    }
+    else{
+        echo "eshte qift";
+    }
+}
+is_qift(156);
+
+function det3(){
 
 
 
-
-
-
-
+    
+}
+det3();
 
 
 
---
+++ php/moduli4/text.php
@@ -0,0 +1,15 @@
+<?php
+$sum = 1.5 + 1.5;
+
+echo $sum;
+echo "<br>";
+echo gettype($sum);
+echo "<br>";
+$students = ["Bledi", "Olsa", "dorant"];
+print_r($students);
+echo "<br>";
+echo phpversion();
+phpinfo();
+
+
+?>
