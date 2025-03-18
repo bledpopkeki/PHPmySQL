7b2d84eb85cd1f36cd637eff865bbf4fa41f66a7
bledpopkeki <bledh4352@gmail.com>
2025-03-18 16:20:22 +0100

    12324

--- php/moduli11/add.php
+++ php/moduli11/add.php
@@ -1,15 +1,14 @@
 <?php
 include_once('config.php');
-if(isset($_POST["submit"])){
-    $name = $_POST["name"];
-    $surname = $_POST["surname"];
-    $email = $_POST["email"];
-    $age = $_POST["age"];
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $email = $_POST['email'];
+    $age = $_POST['age'];
 
-    $sql = "INSERT INTO `users`(`id`, `name`, `surname`, `email`, `age`) VALUES ('[:id]','[:name]','[:surname]','[:email]','[:age]')";
+    $sql = "insert into users(name, surname, email, age) values (:name, :surname, :email, :age)";
     $sqlQuery = $conn->prepare($sql);
 
-    $sqlQuery->bindParam(':id', $id);
     $sqlQuery->bindParam(':name', $name);
     $sqlQuery->bindParam(':surname', $surname);
     $sqlQuery->bindParam(':email', $email);
@@ -17,21 +16,9 @@ if(isset($_POST["submit"])){
 
     $sqlQuery->execute();
 
-    echo "data saved succsesfully...";
-
-
-
-
-
-
-
-
-
+    echo "Data saved succsesfully ...";
 
 
 
 }
-
-
-
 ?>
--- php/moduli11/dashboard.php
+++ php/moduli11/dashboard.php
@@ -3,8 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>lesson 10</title>
 </head>
+<style>
+    table,td,th{
+        border:1px solid black;
+        border-collapse: collapse;
+    }
+    td,th{
+        padding:10px 20px;
+    }
+</style>
 <body>
 <?php
 include_once('config.php');
@@ -14,31 +23,30 @@ $getUsers->execute();
 $users = $getUsers->fetchAll();
 ?>
 <table>
-<thead>
-    <th>Id</th>
-    <th>Name</th>
-    <th>Surname</th>
-    <th>Email</th>
-    <th>Age</th>
-</thead>
+    <thead>
+        <th>id</th>
+        <th>name</th>
+        <th>surname</th>
+        <th>email</th>
+        <th>age</th>
+    </thead>
 <tbody>
     <?php
     foreach($users as $user){
-    ?>
-    <tr>
-        <td><?=$user["id"]?></td>
-        <td><?=$user["name"]?></td>
-        <td><?=$user["surname"]?></td>
-        <td><?=$user["email"]?></td>
-        <td><?=$user["age"]?></td>
+        ?>
+        <tr>
+            <td><?= $user['id']?></td>
+            <td><?= $user['name']?></td>
+            <td><?= $user['surname']?></td>
+            <td><?= $user['email']?></td>
+            <td><?= $user['age']?></td>
     </tr>
     <?php
     }
     ?>
 </tbody>
-
-
-
+</table>
+<a href="index.php">Add User</a>
 </table>
 </body>
 </html>
