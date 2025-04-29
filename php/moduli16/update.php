6aceda8b744dd039a7c4b5d7be19a57013a394f7
bledpopkeki <bledh4352@gmail.com>
2025-04-29 07:12:34 -0700

    sdfsdf

---
+++ php/moduli16/update.php
@@ -0,0 +1,16 @@
+<?php
+include_once('config.php');
+if(isset($_POST['submit'])){
+    $id=$_POST['id'];
+    $movie_name=$_POST['movie_name'];
+    $movie_desc=$_POST['movie_desc'];
+    $movie_quality=$_POST['movie_quality'];
+    $sql="UPDATE movies SET id=:id,movie_name=:movie_name,movie_desc=:movie_desc,movie_quality=:movie_quality WHERE id=:id";
+    $prep=$conn->prepare($sql);
+    $prep->bindParam(':id',$id);
+    $prep->bindParam(':movie_name',$movie_name);
+    $prep->bindParam(':movie_desc',$movie_desc);
+    $prep->bindParam(':movie_quality',$movie_quality);
+    $prep->execute();
+    header('Location:list_movies.php');
+}
