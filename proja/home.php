8c2f79f940d3299f8dc8b62979449e5714f0d4af
bledpopkeki <bledh4352@gmail.com>
2025-05-20 07:02:39 -0700

    345672345978234789734

--- proja/home.php
+++ proja/home.php
@@ -1,32 +1,40 @@
-<?php
-include_once('config.php');
+<?php 
+ /*
+  We will include config.php for connection with database.
+  We will fetch all datas from movies in database and show them.
+  */
+	
+   include_once('config.php');
 
-$sql="SELECT * FROM races";
-$selectRaces=$conn->prepare($sql);
-$selectRaces->execute();
+   $sql = "SELECT * FROM movies";
+   $selectMovies = $conn->prepare($sql);
+   $selectMovies->execute();
+   $movies_data = $selectMovies->fetchAll();
 
-$races_data=$selectRaces->fetchALl();
 
-?>
-<!DOCTYPE html>
+
+ ?>
+
+ <!DOCTYPE html>
  <html>
  <head>
-  <title>Home</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
+ 	<title>Home</title>
+ 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+ 	 <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.88.1">
-    <link rel="apple-touch-icon" href="/docs/5.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-  <link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-  <link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-  <link rel="manifest" href="/docs/5.1/assets/img/favicons/manifest.json">
-  <link rel="mask-icon" href="/docs/5.1/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
-  <link rel="icon" href="/docs/5.1/assets/img/favicons/favicon.ico">
-  <meta name="theme-color" content="#7952b3">
+  	<link rel="apple-touch-icon" href="/docs/5.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+	<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+	<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+	<link rel="manifest" href="/docs/5.1/assets/img/favicons/manifest.json">
+	<link rel="mask-icon" href="/docs/5.1/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+	<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon.ico">
+	<meta name="theme-color" content="#7952b3">
  </head>
  <body>
- <header>
+
+ 	<header>
   <div class="collapse bg-dark" id="navbarHeader">
     <div class="container">
       <div class="row">
@@ -58,59 +66,56 @@ $races_data=$selectRaces->fetchALl();
   </div>
 </header>
  
-  <section class="py-5 text-center container">
+ 	<section class="py-5 text-center container">
     <div class="row py-lg-5">
       <div class="col-lg-6 col-md-8 mx-auto">
         <h1 class="fw-light">Album example</h1>
         <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
         <p>
-        <a href="#" class="btn btn-primary my-2">Main call to action</a>
+          <a href="#" class="btn btn-primary my-2">Main call to action</a>
           <a href="#" class="btn btn-secondary my-2">Secondary action</a>
         </p>
       </div>
     </div>
   </section>
 
-
   <div class="album py-5 bg-light">
     <div class="container">
 
-
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-      <?php
-    foreach($races_data as $race_data){
-    ?>
-    <div class="col">
-      <div class="card shadow-sm">
-        <img src="race_images/<?php echo $race_data['race_image'];?>" height="350">
-        <div class="card-body">
-          <h4><?php echo $race_data['race_name'] ?></h4>
-          <p class="card-text"><?php echo $race_data['race_time'] ?></p>
-          <div class="d-flex justify-content-between align-items-center">
-            <div class="btn-group">
-             <a href="details.php?id=<?php echo $race_data['id'];?>" class="btn btn-sm btn-outline-secondary">View</a>
-             <a href="edit.php?id=<?php echo $race_data['id'];?>" class="btn btn-sm btn-outline-secondary">Edit</a>
+
+      	<?php foreach ($movies_data as $movie_data) { ?>
+
+      	<div class="col">
+          <div class="card shadow-sm">
+
+            <img src="movie_images/<?php echo $movie_data['movie_image'];  ?>" height="350">
+
+            <div class="card-body">
+              <h4><?php echo $movie_data['movie_name']; ?></h4>
+              <p class="card-text"><?php echo $movie_data['movie_desc']; ?></p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="details.php?id=<?php echo $movie_data['id']; ?>"  class="btn btn-sm btn-outline-secondary" >View</a>
+                  <a href="edit.php?id=<?php echo $movie_data['id']; ?>"  class="btn btn-sm btn-outline-secondary">Edit</a>
+                </div>
+                <small class="text-muted">Rating: <?php echo $movie_data['movie_rating']; ?></small>
+                <small class="text-muted"><?php echo $movie_data['movie_quality']; ?></small>
+              </div>
             </div>
-            <small class="text-muted">Date:<?php echo $race_data['race_date']; ?></small>
-            <small class="text-muted"><?php echo $race_data['race_price']; ?></small>
           </div>
         </div>
+      		
+      <?php	} ?>
+   
+       
 
-
-      </div>
-    </div>
-
-
-    <?php
-    }
-    ?>
+        
       </div>
     </div>
   </div>
 
-
   
-
 
 
  </body>
